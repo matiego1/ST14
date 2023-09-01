b6b9791b08de7e208c129741d663fd3d37178266
Matiego <filipowicz.mati@gmail.com>
2023-09-01 08:52:52 +0200

    new features, bug fixes

--- src/main/java/me/matiego/st14/listeners/PlayerResourcePackStatusListener.java
+++ src/main/java/me/matiego/st14/listeners/PlayerResourcePackStatusListener.java
@@ -1,6 +1,7 @@
 package me.matiego.st14.listeners;
 
 import me.matiego.st14.Logs;
+import me.matiego.st14.utils.Utils;
 import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
@@ -13,7 +14,10 @@ public class PlayerResourcePackStatusListener implements Listener {
         PlayerResourcePackStatusEvent.Status status = event.getStatus();
         Player player = event.getPlayer();
         switch (status) {
-            case DECLINED -> player.sendMessage("&6Pobierz resource-pack! Dzięki temu łatwiej Ci będzie rozpoznać wszystkie nowe moby.");
+            case DECLINED -> {
+                player.sendMessage(Utils.getComponentByString("&6Pobierz resource-pack! Dzięki temu łatwiej Ci będzie rozpoznać wszystkie nowe moby."));
+                Logs.warning("Gracz " + player.getName() + " nie zezwolił na resource-pack.");
+            }
             case FAILED_DOWNLOAD -> Logs.warning("Graczowi " + player.getName() + " nie udało się pobrać resource-packa! Czy link podany w pliku server.properties jest poprawny?");
         }
     }
